dbdf60b8b998b48d6289312dc8e70123be69e8f9
dantheman8300 <daniel.7039@hotmail.com>
2024-02-16 12:24:35 -0800

    log out

--- airbnb/app/(tabs)/profile.tsx
+++ airbnb/app/(tabs)/profile.tsx
@@ -1,11 +1,22 @@
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 import React from "react";
+import { useAuth } from "@clerk/clerk-expo";
+import { Link } from "expo-router";
 
 
 function Page() {
+  
+  const { signOut, isSignedIn } = useAuth();
+
   return (
     <View>
-      <Text>Profile</Text>
+      <Button title="Log Out" onPress={() => signOut()} />
+      {
+        !isSignedIn && 
+        <Link href='/(modals)/login'>
+          <Text>Login</Text>
+        </Link>
+      }
     </View>
   )
 }
