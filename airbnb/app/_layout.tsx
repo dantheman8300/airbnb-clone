b6d8fddf4b5e1a4ec68dc0e9267c1d632cc82174
dantheman8300 <daniel.7039@hotmail.com>
2024-02-16 11:26:01 -0800

    modal style + router injection

--- airbnb/app/_layout.tsx
+++ airbnb/app/_layout.tsx
@@ -1,18 +1,24 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import { Ionicons } from "@expo/vector-icons";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack, useRouter } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import { TouchableOpacity } from "react-native";
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from 'expo-router';
+} from "expo-router";
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: "(tabs)",
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -20,9 +26,9 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    'mon': require('@/assets/fonts/Montserrat-Regular.ttf'),
-    'mon-sb': require('@/assets/fonts/Montserrat-SemiBold.ttf'),
-    'mon-b': require('@/assets/fonts/Montserrat-Bold.ttf'),
+    mon: require("@/assets/fonts/Montserrat-Regular.ttf"),
+    "mon-sb": require("@/assets/fonts/Montserrat-SemiBold.ttf"),
+    "mon-b": require("@/assets/fonts/Montserrat-Bold.ttf"),
   });
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
@@ -44,10 +50,30 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
+  
+  const router = useRouter();
 
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="(modals)/login"
+        options={{
+          title: 'Log in or sign up',
+          presentation: "modal",
+          headerTitleStyle: {
+            fontFamily: "mon-sb",
+          },
+          headerLeft: () => (
+            <TouchableOpacity 
+              onPress={() => router.back()}
+              style={{ padding: 10 }}
+            >
+              <Ionicons name="close-outline" size={28} />
+            </TouchableOpacity>
+          )
+        }}
+      />
     </Stack>
   );
 }
